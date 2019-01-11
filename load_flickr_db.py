329165ef9ef73931f2efa735f12d8f298974f820
sukurcf <sukurcf@gmail.com>
2019-01-12 00:23:46 +0530

    populated db

--- load_flickr_db.py
+++ load_flickr_db.py
@@ -4,7 +4,7 @@ import flickrapi
 api_key = u'3aad6f4e25e8f7c81bf7866d8517901b'
 api_secret = u'be22bfff3db1e98f'
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
-groups = ['80641914@N00', '906097@N21', '96035807@N00']
+groups = ['80641914@N00', '906097@N21', '96035807@N00', '34427469792@N01', '16978849@N00']
 for groupid in groups:
     photos = flickr.groups.pools.getPhotos(group_id=groupid)['photos']['photo']
     conn = sqlite3.connect('infilect_project/db.sqlite3')
