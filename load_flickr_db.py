1cf97b577771a55591d41afd1ad179df8120824e
sukurcf <sukurcf@gmail.com>
2019-01-11 23:59:33 +0530

    updated models and script is populating db

--- load_flickr_db.py
+++ load_flickr_db.py
@@ -4,14 +4,15 @@ import flickrapi
 api_key = u'3aad6f4e25e8f7c81bf7866d8517901b'
 api_secret = u'be22bfff3db1e98f'
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
-photos = flickr.groups.pools.getPhotos(group_id='80641914@N00')
-conn = sqlite3.connect('infilect_project/db.sqlite3')
-c = conn.cursor()
-columns = 'id, owner, secret, server, farm, title, ispublic, isfriend, isfamily, dateadded, ownername'
-photos = photos['photos']['photo']
-for i in photos:
-    data = (i['id'], i['owner'], i['secret'], i['server'], i['farm'], i['title'],
-            i['ispublic'], i['isfriend'], i['isfamily'], i['dateadded'], i['ownername'])
-    c.execute(
-        f"insert into flickrdb_fphoto ({columns}) values (?,?,?,?,?,?,?,?,?,?,?)", data)
-conn.commit()
+groups = ['80641914@N00', '906097@N21', '96035807@N00']
+for groupid in groups:
+    photos = flickr.groups.pools.getPhotos(group_id=groupid)['photos']['photo']
+    conn = sqlite3.connect('infilect_project/db.sqlite3')
+    c = conn.cursor()
+    columns = 'id, owner, secret, server, farm, title, ispublic, isfriend, isfamily, dateadded, ownername, groupid'
+    for i in photos:
+        data = (i['id'], i['owner'], i['secret'], i['server'], i['farm'], i['title'],
+                i['ispublic'], i['isfriend'], i['isfamily'], i['dateadded'], i['ownername'], groupid)
+        c.execute(
+            f"insert into flickrdb_fphoto ({columns}) values (?,?,?,?,?,?,?,?,?,?,?,?)", data)
+    conn.commit()
