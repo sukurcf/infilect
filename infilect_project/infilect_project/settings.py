868ba154e47f0922f7264b6b3e22929b0bc27c80
sukurcf <sukurcf@gmail.com>
2019-01-14 20:45:55 +0530

    added connection.close in load_flickrdb script

--- infilect_project/infilect_project/settings.py
+++ infilect_project/infilect_project/settings.py
@@ -59,7 +59,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 ROOT_URLCONF = 'infilect_project.urls'
